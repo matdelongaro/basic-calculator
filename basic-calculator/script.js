6b5df6d5d052e3f55ad966306e5b31d57f7b9ca8
matias delongaro <matiasadelongaro@gmail.com>
2024-05-09 12:48:56 -0300

    subiendo al repositorio

---
+++ basic-calculator/script.js
@@ -0,0 +1,14 @@
+
+function add(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+function reset(){
+    document.getElementById('pantalla').value = ''
+}
+
+function calculate(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
