387c0dc405eb3d77e8093896c947ad6856ec5727
matias delongaro <matiasadelongaro@gmail.com>
2024-05-09 19:42:50 -0300

    update

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -1,14 +1,13 @@
-
 function add(valor){
-    document.getElementById('pantalla').value += valor
+    document.getElementById('pantalla').value += valor;
 }
 
 function reset(){
-    document.getElementById('pantalla').value = ''
+    document.getElementById('pantalla').value = '';
 }
 
 function calculate(){
-    const valorPantalla = document.getElementById('pantalla').value
-    const resultado = eval(valorPantalla)
-    document.getElementById('pantalla').value = resultado
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = eval(valorPantalla);
+    document.getElementById('pantalla').value = resultado;
 }
